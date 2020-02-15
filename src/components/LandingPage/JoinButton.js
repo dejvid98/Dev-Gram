9d75f671b7d59434a06ee32eed510b33c893ec4f
David Sucur <davidsucur111@gmail.com>
2020-02-16 00:00:21 +0100

    Update v13.0

---
+++ src/components/LandingPage/JoinButton.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const JoinButton = () => {
+    return (
+        <div className="join-buttons">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default JoinButton
--- src/components/LandingPage/LangdingPage.js
+++ src/components/LandingPage/LangdingPage.js
@@ -15,24 +15,35 @@ const LangdingPage = props => {
             <Navbar />
             {isLoggedIn ? null : (
                 <div className="join-buttons">
-                    <Link to="/register" style={{ textDecoration: 'none' }}>
-                        <Button
-                            className="join-button"
-                            variant="contained"
-                            style={{
-                                backgroundColor: '#1DE9B6',
-                                height: '4rem',
-                                borderRadius: '2rem',
-                                fontSize: '2rem',
-                                textAlign: 'center',
-                                color: 'white',
-                                width: '13rem',
-
-                            }}
-                        >
-                            JOIN US
-                        </Button>
-                    </Link>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div className="join-button">
+                        <Link to="/register" style={{ textDecoration: 'none' }}>
+                            <Button
+                                className="join-button"
+                                variant="contained"
+                                style={{
+                                    backgroundColor: '#1DE9B6',
+                                    height: '4rem',
+                                    borderRadius: '2rem',
+                                    fontSize: '2rem',
+                                    textAlign: 'center',
+                                    color: 'white',
+                                    width: '13rem',
+                                }}
+                            >
+                                JOIN US
+                            </Button>
+                        </Link>
+                    </div>
                 </div>
             )}
         </div>
